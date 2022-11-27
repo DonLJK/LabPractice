59893cd25eba07410b087ca6fd19ceca37b06ccc
Don <donovanlimjk18.21@ichat.sp.edu.sg>
2022-11-27 19:03:29 +0800

    v2.2

--- LabPractice2.1.py
+++ LabPractice2.1.py
@@ -1,9 +1,12 @@
+import statistics
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Intro to Python")
     v1 = get_user_input()
     display_main_menu(v1)
     calculate_average(v1)
     calc_min_max_temperature(v1)
+    calc_med(v1)
+    calc_median_temperature(v1)
 
 
 
@@ -31,6 +34,23 @@ def calc_min_max_temperature(v1):
     print("The Minimum is: ", minimum)
     print("The Maximum is: ", maximum)
     return minimum, maximum
+
+def calc_median_temperature(v1):
+    x = sorted(v1)
+    print("This is the sorted value in ascending order: ", x)
+    y = len(v1)
+    if y % 2 == 0:
+        med1= x[y//2]
+        med2= x[y//2-1]
+        med = (med1 + med2)/2
+    else:
+        med = x[y//2]
+    print("median is: " + str(med))
+
+def calc_med(v1):
+    x = sorted(v1)
+    y = statistics.median(x)
+    print("The median value: ", y)
 
 
 if __name__ == "__main__":
